7792eef5c93ba556a4cf89975174f8994430a46d
fassbender1 <civello6969@gmail.com>
2026-01-21 18:07:23 +0200

    g

--- urls_views/department/urls.py
+++ urls_views/department/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from department.views import index
 
 urlpatterns = [
-    path('', index),
+    path('department/<id>/', index),
+    path('department/<int:id>/', index),
+
 ]
--- urls_views/department/views.py
+++ urls_views/department/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request: http.HttpRequest) -> http.HttpResponse:
-    return http.HttpResponse("Hello, world. You're the department!", content_type="text/plain")
+def index(request: http.HttpRequest, id: int) -> http.HttpResponse:
+    return http.HttpResponse(f"The ID is {id}", content_type="text/plain")
