ee0af5252ae619e5c71a5a122577cf13f90f3937
fassbender1 <civello6969@gmail.com>
2026-02-14 19:37:50 +0200

    g

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 
 def home_page(request: HttpRequest) -> HttpResponse:
     return render(request, 'common/home-page.html')
+
+
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -6,11 +6,11 @@ from django.shortcuts import render
 def pet_add(request: HttpRequest) -> HttpResponse:
     return render(request, 'pets/pet-add-page.html')
 
-def pet_details(request: HttpRequest, pk:int) -> HttpResponse:
+def pet_details(request: HttpRequest, username: str, pet_slug:str) -> HttpResponse:
     return render(request, 'pets/pet-details-page.html')
 
-def pet_edit(request: HttpRequest, pk:int) -> HttpResponse:
+def pet_edit(request: HttpRequest, username: str, pet_slug:str) -> HttpResponse:
     return render(request, 'pets/pet-edit-page.html')
 
-def pet_delete(request: HttpRequest, pk:int) -> HttpResponse:
+def pet_delete(request: HttpRequest, username: str, pet_slug:str) -> HttpResponse:
     return render(request, 'pets/pet-delete-page.html')
